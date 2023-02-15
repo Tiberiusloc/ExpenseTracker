c1905f141c1a037c6f5f6644de65b16ae406eb4c
Tiberius Lockett <tlockett680@gmail.com>
2023-02-14 17:42:22 -0800

    add expenseitem demo, add css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from "./components/ExpenseItem";
+import './components/ExpenseItem.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Tiberius
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="start">
+        <h2>Let's get started!</h2>
+        <ExpenseItem />
+      </div>
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <h2>Car</h2>
+      <div className="expense-item__price">$10</div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
