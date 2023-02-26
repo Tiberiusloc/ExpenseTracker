73f88556bf07b5cb9a6147231a2d39eac6915072
Tiberius Lockett <tlockett680@gmail.com>
2023-02-26 08:33:02 -0800

    added css and components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,19 @@
 import './ExpenseItem.css';
+import Card from './Card';
+import ExpenseDate from './ExpenseDate';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+  const { title, amount } = props;
+
   return (
-    <div className="expense-item">
-      <h2>Car</h2>
-      <div className="expense-item__price">$10</div>
-    </div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+          <div className="expense-item__description">
+            <h2>{title}</h2>
+          </div>
+          <div className="expense-item__price">{amount}
+        </div>
+    </Card>
   );
 }
 
