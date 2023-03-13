3b62a69f750d7bc03e83bc4ac8b90602a155d243
Tiberius Lockett <tlockett680@gmail.com>
2023-03-12 21:44:40 -0500

    add expenses list to take in user input

--- src/App.js
+++ src/App.js
@@ -1,35 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseDisplay from "./components/Expenses/ExpenseDisplay";
 import NewExpense from "./components/NewExpense/NewExpense";
 import './components/Expenses/ExpenseItem.css';
 
-function App() {
-
-  const expenses = [
+const dummyExpenses = [
     {
       id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
+      title: 'Add a new expense',
+      amount: 0.01,
+      date: new Date(),
     },
   ];
 
+function App() {
+
+  const [expenses, setExpenses] = useState(dummyExpenses)
+
+
   const addExpenseHandler = expense => {
-    console.log(expense)
+    setExpenses(prevExpenses => {
+      return [expense, prevExpenses];
+    } );
   };
 
   return (
